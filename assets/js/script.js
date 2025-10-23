5dce676c0e2b843258ff2ea4a81c485991d3549b
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-23 17:05:06 +0200

    Add -for loop
    Add API query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 const ulEl = document.createElement("ul")
 //Creo, solo in memoria, il nodo <ul>
-
+for(let i = 0; i <10; i++){
+//Ciclo for che vanza 10 volte
+   axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+   //interrogazione dell'API
 
