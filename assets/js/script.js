ebfef8a604fcea745b13770308a84713c3df423c
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-23 17:10:49 +0200

    Add the <li> node
    Change the innerText of the <li> node
    
    Assign the <il> node as a child of the <ul> node
    
    Append the <ul> node to the document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-const ulEl = document.createElement("ul")
+const ulEl = document.createElement("ul");
 //Creo, solo in memoria, il nodo <ul>
-for(let i = 0; i <10; i++){
-//Ciclo for che vanza 10 volte
-   axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-   //interrogazione dell'API
-   .then(function(response){
-    //-.then si attiva solo se la promise dell'interrogazione è risolta
-    const mail = response.data.response
-    /*Response contiene un oggetto, risultato dell'interrogazione, passato a lui da -.then.
+for (let i = 0; i < 10; i++) {
+  //Ciclo for che vanza 10 volte
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    //interrogazione dell'API
+    .then(function (response) {
+      //-.then si attiva solo se la promise dell'interrogazione è risolta
+      const mail = response.data.response;
+      /*Response contiene un oggetto, risultato dell'interrogazione, passato a lui da -.then.
       All'interno dell'oggetto estraggo ciò che serve
     */
-
+      const liEl = document.createElement("li");
+      //Creo un nodo <li>
+      liEl.innerText = mail;
+      //Muto l'innerText del nodo <li>
+      ulEl.appendChild(liEl);
+      //Assegno il nodo <li> come figlio del nodo <ul>
+    });
+  document.body.appendChild(ulEl);
+  //Appendo il nodo <ul>, e relativi figli, al documento
+}
